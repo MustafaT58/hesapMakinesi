b626cbb4c3be7b5f8e039ba54f2a23a76b88b03c
SEM <SEM@DESKTOP-4GKM6HK>
2022-09-26 14:37:29 +0300

    Add project files.

---
+++ hesapMakinesi/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesapMakinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal birinciSayi = cevir("Birinci sayıyı giriniz = ");
+            decimal ikinciSayi = cevir("İkinci sayıyı giriniz = ");
+            Console.Write("İşlem Giriniz'toplama(+),çıkarma(-),çarpma(*),bölme(/)' = ");
+            string islem = Console.ReadLine();
+            decimal snc;
+            if(islem=="+")
+            {
+                snc= topla(birinciSayi, ikinciSayi);
+                Sonuc(snc);
+
+            }
+            else if(islem=="-")
+            {
+                snc = cikar(birinciSayi, ikinciSayi);
+                Sonuc(snc);
+            }
+            else if (islem == "*")
+            {
+                snc = carp(birinciSayi, ikinciSayi);
+                Sonuc(snc);
+            }
+            else if (islem == "/")
+            {
+                snc = bol(birinciSayi, ikinciSayi);
+                Sonuc(snc);
+            }
+            else
+            {
+                Console.WriteLine("Hatalı Giriş!");
+            }
+            Console.ReadLine();
+        }
+        static decimal cevir(string deger)
+        {
+                Console.Write(deger);
+                return Convert.ToDecimal(Console.ReadLine());
+        }
+        static decimal topla(decimal x, decimal y)
+        {
+            return x+y;
+        }
+        static decimal cikar(decimal x, decimal y)
+        {
+            return x - y;
+        }
+        static decimal carp(decimal x, decimal y)
+        {
+            return x * y;
+        }
+        static decimal bol(decimal x, decimal y)
+        {
+            return x / y;
+        }
+        static void Sonuc(decimal sonuc)
+        {
+            Console.Write("Toplam Değer = " + sonuc);
+        }
+    }
+}
---
+++ hesapMakinesi2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesapMakinesi2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            decimal birinciSayi = cevir("Birinci sayıyı giriniz = ");
+            decimal ikinciSayi = cevir("İkinci sayıyı giriniz = ");
+            decimal snc = islemYap(birinciSayi, ikinciSayi);
+            Console.WriteLine("Sonuc = " + snc);
+            Console.ReadLine();
+        }
+        public static decimal cevir(string deger)
+        {
+            Console.Write(deger);
+            return Convert.ToDecimal(Console.ReadLine());
+        }
+        public static decimal islemYap(decimal x, decimal y)
+        {
+            decimal sonuc = 0;
+            string isKod = kodAl();
+            switch (isKod)
+            {
+                case "+": sonuc = x + y; break;
+                case "-": sonuc = x - y; break;
+                case "*": sonuc = x * y; break;
+                case "/": sonuc = x / y; break;
+                default: Console.WriteLine("Yanlış İşlem Kodu"); return islemYap(x, y); break;
+            }
+            return sonuc;
+        }
+        public static string kodAl()
+        {
+            Console.WriteLine("İşlem Kodu= ");
+            return Console.ReadLine();
+        }
+
+    }
+}
